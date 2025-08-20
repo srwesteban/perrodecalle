42afa1f8dc78c53b53358a1e88a7c401a5d197be
srwesteban <sr.w@hotmail.com>
2025-08-20 10:54:40 -0500

    fondo

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,6 +1,33 @@
+import Particles from "./Particles";
+import fondo from "../assets/img/dog.jpg"; // 👈 importa la imagen
+
 function Background() {
   return (
-    <div className="absolute inset-0 bg-gradient-to-b from-gray-900 to-gray-800 z-0" />
+    <div
+      style={{
+        width: "100%",
+        height: "100vh", // 👈 ahora ocupa toda la pantalla
+        position: "absolute",
+        top: 0,
+        left: 0,
+        backgroundImage: `url(${fondo})`, // 👈 corregido
+        backgroundSize: "cover",          // ajusta la imagen al tamaño de la pantalla
+        backgroundPosition: "center",     // centra la imagen
+        backgroundRepeat: "no-repeat",
+        zIndex: -1, // para que quede detrás
+      }}
+    >
+      <Particles
+        particleColors={["#ffffff", "#ffffff"]}
+        particleCount={200}
+        particleSpread={10}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover={true}
+        alphaParticles={false}
+        disableRotation={false}
+      />
+    </div>
   );
 }
 
