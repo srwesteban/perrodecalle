85278d0f1cd1cb31add431a9bc87264455aec9ab
srwesteban <sr.w@hotmail.com>
2025-08-21 10:43:15 -0500

    m

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'   // 👈 importa el plugin
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [react(), tailwind()],          // 👈 agrégalo aquí
+})
