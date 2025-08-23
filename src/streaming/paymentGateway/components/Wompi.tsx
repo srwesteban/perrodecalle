aafa48b9e4040c3c6103112ec0c8f270f7b429e8
srwesteban <sr.w@hotmail.com>
2025-08-23 10:55:12 -0500

    ya?

--- src/streaming/paymentGateway/components/Wompi.tsx
+++ src/streaming/paymentGateway/components/Wompi.tsx
@@ -4,22 +4,29 @@ import WompiButton from "./Wompibutton";
 export default function Wompi() {
   const reference = useMemo(() => `DON-${Date.now()}`, []);
   const redirectUrl =
-    typeof window !== "undefined" ? `${window.location.origin}/gracias` : undefined;
+    typeof window !== "undefined"
+      ? `${window.location.origin}/gracias`
+      : undefined;
 
   // Montos en pesos (se convierten a centavos más abajo)
   const amounts = [1500, 2000, 5000, 10000, 20000, 65000];
 
   return (
-    <div className="flex flex-wrap gap-2">
-      {amounts.map((cop) => (
-        <WompiButton
-          key={cop}
-          amountInCents={cop * 100}   // 1.500 → 150000 centavos
-          currency="COP"
-          reference={reference}
-          redirectUrl={redirectUrl}
-        />
-      ))}
-    </div>
+    <>
+      <div>
+        <p>Selecciona la donación que deseas hacer:</p>
+      </div>
+      <div className="flex flex-wrap gap-2">
+        {amounts.map((cop) => (
+          <WompiButton
+            key={cop}
+            amountInCents={cop * 100} // 1.500 → 150000 centavos
+            currency="COP"
+            reference={reference}
+            redirectUrl={redirectUrl}
+          />
+        ))}
+      </div>
+    </>
   );
 }
