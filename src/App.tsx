2a71c5450cc4abb6dcae953ee10c5f8da5b43ab7
srwesteban <sr.w@hotmail.com>
2025-08-26 16:48:30 -0500

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import MobileIntro from "./components/MobileIntro";
 import SplashScreen from "./components/SplashScreen";
 
 import LiveVideo from "./components/LiveVideo";
-import Wompi from "./streaming/paymentGateway/components/Wompi";
 import MediaFeed from "./components/MediaFeed";
 import ProgressBar from "./components/ProgressBar";
 import Historial from "./components/Historial";
@@ -23,18 +22,11 @@ import DonationSection from "./components/DonationSection";
 import Informacion from "./components/Informacion";
 
 function App() {
-  const assetsReady = usePreloadImages([
-    portada,
-    perroGif,
-    imagenEncima,
-    click,
-  ]);
+  const assetsReady = usePreloadImages([portada, perroGif, imagenEncima, click]);
   const [introDone, setIntroDone] = useState(false);
 
   // 1. Splash → hasta que carguen los assets
-  if (!assetsReady) {
-    return <SplashScreen />;
-  }
+  if (!assetsReady) return <SplashScreen />;
 
   // 2. MobileIntro → bloquea hasta terminar fade
   if (!introDone) {
@@ -45,18 +37,36 @@ function App() {
         gifClick={click}
         durationMs={600}
         imagenDos={imagenEncima}
-        onFinish={() => setIntroDone(true)} // 👈 notificamos cuando termina
+        onFinish={() => setIntroDone(true)}
       />
     );
   }
 
   // 3. App real
   return (
-    <div className="relative min-h-screen md:h-dvh md:overflow-hidden overflow-x-hidden">
+    <div
+      className={[
+        "relative min-h-screen",
+        // 👇 Scroll SOLO en pantallas pequeñas
+        "overflow-y-auto md:overflow-y-hidden",
+        "overflow-x-hidden",
+        // Altura 100dvh en desktop para grid perfecto
+        "md:h-dvh",
+      ].join(" ")}
+    >
       <Background />
 
       <div className="relative z-10 text-white p-2 md:p-4 h-full">
-        <div className="flex flex-col gap-2 md:grid md:grid-cols-3 md:grid-rows-8 md:gap-3 md:h-full">
+        <div
+          className={[
+            "flex flex-col gap-2",
+            "md:grid md:grid-cols-3 md:gap-3 md:h-full",
+            // 👇 Filas robustas sin depender de la config del amigo
+            "md:[grid-template-rows:repeat(8,minmax(0,1fr))]",
+            // evita que hijos estiren el grid
+            "min-h-0",
+          ].join(" ")}
+        >
           {/* Bloque del LiveVideo */}
           <div
             className={[
@@ -70,18 +80,22 @@ function App() {
               <StatsBar />
             </div>
 
-            {/* Contenedor del video: crece y no recorta */}
+            {/* Contenedor del video: crece y no recorta ni invade filas */}
             <div className="flex-1 min-h-0">
               <LiveVideo />
             </div>
           </div>
 
+          {/* Fila extra de información (independiente) */}
           <Informacion
             className={[
-              "order-2 md:col-span-2 md:row-span-1 md:col-start-1 md:row-start-5",
+              "order-2",
+              "md:col-span-2 md:row-span-1 md:col-start-1 md:row-start-5",
+              "min-h-0", // por si el contenido crece
             ].join(" ")}
           />
 
+          {/* Panel Donaciones */}
           <div
             className={[
               "order-3 bg-black/40 rounded-xl p-3 min-h-[200px]",
@@ -94,17 +108,31 @@ function App() {
               <ProgressBar goal={1000000} />
             </div>
 
-            {/* Scroll interno */}
+            {/* Scroll interno solo en esta columna */}
             <div className="flex-1 min-h-0 overflow-y-auto">
               <DonationSection />
             </div>
           </div>
 
-          <div className="order-4 bg-black/40 rounded-xl p-3 min-h-[220px] md:col-span-3 md:row-span-3 md:col-start-2 md:row-start-6">
+          {/* Historial */}
+          <div
+            className={[
+              "order-4 bg-black/40 rounded-xl p-3 min-h-[220px]",
+              "md:col-span-3 md:row-span-3 md:col-start-2 md:row-start-6",
+              "min-h-0",
+            ].join(" ")}
+          >
             <Historial />
           </div>
 
-          <div className="order-5 bg-black/40 rounded-xl p-3 md:col-span-1 md:row-span-3 md:col-start-1 md:row-start-6">
+          {/* Media Feed */}
+          <div
+            className={[
+              "order-5 bg-black/40 rounded-xl p-3",
+              "md:col-span-1 md:row-span-3 md:col-start-1 md:row-start-6",
+              "min-h-0",
+            ].join(" ")}
+          >
             <MediaFeed />
           </div>
         </div>
